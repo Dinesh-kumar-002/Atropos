df592cd085ff483ad9795fe828592934d7dbf4d6
dkdon812002@gmail.com <dkdon812002@gmail.com>
2023-08-11 12:54:36 +0530

    message

---
+++ script.js
@@ -0,0 +1,10 @@
+var card=document.querySelector('.card');
+var tiger=document.querySelector('.tiger');
+card.addEventListener('mouseenter', function(){
+    tiger.style.top='20%';
+    tiger.style.right='30px';
+})
+var third=document.querySelector('.third');
+third.addEventListener('mouseover',()=>{
+    
+})
